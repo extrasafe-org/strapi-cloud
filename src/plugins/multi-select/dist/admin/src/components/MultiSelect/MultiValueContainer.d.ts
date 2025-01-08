ecb7cc8e40a541aab61cda0626a23c73443c0b89
AmaseevR <amaseevr@gmail.com>
2025-01-08 22:15:25 +0100

    build plugin through postinstall

---
+++ src/plugins/multi-select/dist/admin/src/components/MultiSelect/MultiValueContainer.d.ts
@@ -0,0 +1,8 @@
+declare const _default: ({ selectProps, data, }: {
+    selectProps: any;
+    data: {
+        value: string;
+        label: string;
+    };
+}) => import("react/jsx-runtime").JSX.Element;
+export default _default;
---
+++ src/plugins/multi-select/dist/admin/src/utils/prefixPluginTranslations.d.ts
@@ -0,0 +1,3 @@
+type TradOptions = Record<string, string>;
+export declare const prefixPluginTranslations: (trad: TradOptions, pluginId: string) => TradOptions;
+export {};
