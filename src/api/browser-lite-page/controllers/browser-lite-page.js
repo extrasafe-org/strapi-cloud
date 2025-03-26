07a18723e89d28d966e10212441ca92719c7fc23
Yana <yanaegorova69.3@gmail.com>
2025-03-26 15:34:26 +0200

    test

--- src/api/browser-lite-page/controllers/browser-lite-page.js
+++ src/api/browser-lite-page/controllers/browser-lite-page.js
@@ -6,27 +6,32 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-// module.exports = createCoreController('api::browser-lite-page.browser-lite-page', ({ strapi }) => ({
-//   async find(ctx) {
-//     try {
-//       const entity = await strapi.entityService.findOne('api::browser-lite-page.browser-lite-page', {
-//         populate: {
-//           HeroSection: {
-//             populate: '*',
-//           },
-//         },
-//       });
+module.exports = createCoreController('api::browser-lite-page.browser-lite-page', ({ strapi }) => ({
+  async find(ctx) {
+    try {
+      const { locale = 'en' } = ctx.query;
 
-//       if (!entity) {
-//         ctx.throw(404, 'Not Found');
-//       }
+      const entity = await strapi.entityService.findOne('api::browser-lite-page.browser-lite-page', {
+        locale, 
+        populate: {
+          HeroSection: { populate: '*' },
+          IconsListSection: { populate: '*' },
+          BannerSection: { populate: '*' },
+          MediaWithTextSection: { populate: '*' },
+          BannerWithButtonSection: { populate: '*' },
+        },
+      });
 
-//       const { createdBy, updatedBy, ...sanitizedEntity } = entity;
+      if (!entity) {
+        return ctx.throw(404, 'Not Found');
+      }
 
-//       return this.transformResponse(sanitizedEntity);
-//     } catch (error) {
-//       ctx.throw(500, 'Internal Server Error', { details: error.message });
-//     }
-//   },
+      const { createdBy, updatedBy, ...sanitizedEntity } = entity;
 
-// }));
+      return this.transformResponse(sanitizedEntity);
+    } catch (error) {
+      console.error('Error in find method:', error);
+      ctx.throw(500, 'Internal Server Error', { details: error.message });
+    }
+  },
+}));
