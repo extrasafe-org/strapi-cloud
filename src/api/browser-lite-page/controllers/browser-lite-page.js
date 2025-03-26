7f46e3c4ee7ae019fc03331181fd2d1be58df9c4
Yana <yanaegorova69.3@gmail.com>
2025-03-26 22:39:33 +0200

    test3

--- src/api/browser-lite-page/controllers/browser-lite-page.js
+++ src/api/browser-lite-page/controllers/browser-lite-page.js
@@ -1,41 +1,41 @@
-// 'use strict';
-
-// const { createCoreController } = require('@strapi/strapi').factories;
-
-// module.exports = createCoreController('api::browser-lite-page.browser-lite-page');
-
 'use strict';
-
-/**
- * browser-lite-page controller
- */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::browser-lite-page.browser-lite-page', ({ strapi }) => ({
-  async find(ctx) {
-    try {
-      // Выполняем запрос к Single Type с полной популяцией всех вложенных данных
-      const entity = await strapi.entityService.findOne('api::browser-lite-page.browser-lite-page', {
-        populate: '*', // Подтягиваем все доступные поля и вложенные данные
-      });
+module.exports = createCoreController('api::browser-lite-page.browser-lite-page');
 
-      // Если запись не найдена, возвращаем 404
-      if (!entity) {
-        return ctx.throw(404, 'No content found');
-      }
+// 'use strict';
 
-      // Убираем системные поля
-      const { createdBy, updatedBy, ...sanitizedEntity } = entity;
+// /**
+//  * browser-lite-page controller
+//  */
 
-      // Форматируем ответ
-      return this.transformResponse(sanitizedEntity);
-    } catch (error) {
-      // Возвращаем подробности ошибки для отладки
-      ctx.throw(500, 'Internal Server Error', {
-        details: error.message,
-        stack: error.stack,
-      });
-    }
-  },
-}));
+// const { createCoreController } = require('@strapi/strapi').factories;
+
+// module.exports = createCoreController('api::browser-lite-page.browser-lite-page', ({ strapi }) => ({
+//   async find(ctx) {
+//     try {
+//       // Минимальный запрос к Single Type без параметров
+//       const entity = await strapi.entityService.findOne('api::browser-lite-page.browser-lite-page', {
+//         populate: '*', // Популяция всех полей первого уровня
+//       });
+
+//       // Проверка результата
+//       if (!entity) {
+//         return ctx.throw(404, 'No content found');
+//       }
+
+//       // Убираем системные поля
+//       const { createdBy, updatedBy, ...sanitizedEntity } = entity;
+
+//       // Возвращаем отформатированный ответ
+//       return this.transformResponse(sanitizedEntity);
+//     } catch (error) {
+//       // Подробный вывод ошибки для диагностики
+//       return ctx.throw(500, 'Internal Server Error', {
+//         details: error.message,
+//         stack: error.stack,
+//       });
+//     }
+//   },
+// }));
