ea4b4b66e4a895c036abe7f0ef02d7a0ffdbecce
Yana <yanaegorova69.3@gmail.com>
2025-03-26 14:25:04 +0200

    remove types and controller for browser lite

--- src/api/browser-lite-page/controllers/browser-lite-page.js
+++ src/api/browser-lite-page/controllers/browser-lite-page.js
@@ -6,41 +6,41 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::browser-lite-page.browser-lite-page', ({ strapi }) => ({
-  async find(ctx) {
-    // Fetch data with population
-    const entities = await strapi.entityService.findMany('api::browser-lite-page.browser-lite-page', {
-      populate: {
-        HeroSection: {
-          populate: '*', 
-        },
-      },
-    });
+// module.exports = createCoreController('api::browser-lite-page.browser-lite-page', ({ strapi }) => ({
+//   async find(ctx) {
+//     // Fetch data with population
+//     const entities = await strapi.entityService.findMany('api::browser-lite-page.browser-lite-page', {
+//       populate: {
+//         HeroSection: {
+//           populate: '*', 
+//         },
+//       },
+//     });
 
-    // Remove unnecessary fields
-    const sanitizedEntities = entities.map(entity => {
-      const { createdBy, updatedBy, ...rest } = entity;
-      return rest;
-    });
+//     // Remove unnecessary fields
+//     const sanitizedEntities = entities.map(entity => {
+//       const { createdBy, updatedBy, ...rest } = entity;
+//       return rest;
+//     });
 
-    return this.transformResponse(sanitizedEntities);
-  },
+//     return this.transformResponse(sanitizedEntities);
+//   },
 
-  async findOne(ctx) {
-    const { id } = ctx.params;
+//   async findOne(ctx) {
+//     const { id } = ctx.params;
 
-    // Fetch data with population
-    const entity = await strapi.entityService.findOne('api::browser-lite-page.browser-lite-page', id, {
-      populate: {
-        HeroSection: {
-          populate: '*', 
-        },
-      },
-    });
+//     // Fetch data with population
+//     const entity = await strapi.entityService.findOne('api::browser-lite-page.browser-lite-page', id, {
+//       populate: {
+//         HeroSection: {
+//           populate: '*', 
+//         },
+//       },
+//     });
 
-    // Remove unnecessary fields
-    const { createdBy, updatedBy, ...sanitizedEntity } = entity;
+//     // Remove unnecessary fields
+//     const { createdBy, updatedBy, ...sanitizedEntity } = entity;
 
-    return this.transformResponse(sanitizedEntity);
-  },
-}));
+//     return this.transformResponse(sanitizedEntity);
+//   },
+// }));
