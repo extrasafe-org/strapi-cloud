bb6a18f3938b4557ef30c625d8011540cb863880
Yana <yanaegorova69.3@gmail.com>
2025-03-26 21:33:52 +0200

    test3

--- src/api/browser-lite-page/controllers/browser-lite-page.js
+++ src/api/browser-lite-page/controllers/browser-lite-page.js
@@ -1,43 +1,51 @@
+// 'use strict';
+
+// /**
+//  * browser-lite-page controller
+//  */
+
+// const { createCoreController } = require('@strapi/strapi').factories;
+
+// module.exports = createCoreController('api::browser-lite-page.browser-lite-page', ({ strapi }) => ({
+//   async find(ctx) {
+//     try {
+ 
+//       const { locale = 'en' } = ctx.query;
+
+    
+//       const entity = await strapi.entityService.findOne('api::browser-lite-page.browser-lite-page', {
+//         locale,
+//         populate: {
+//           HeroSection: { populate: '*' },
+//           IconsListSection: { populate: '*' },
+//           BannerSection: { populate: '*' },
+//           MediaWithTextSection: { populate: '*' },
+//           BannerWithButtonSection: { populate: '*' },
+//         },
+//       });
+
+//       if (!entity) {
+//         return ctx.throw(404, `No content found for locale "${locale}"`);
+//       }
+
+      
+//       const { createdBy, updatedBy, ...sanitizedEntity } = entity;
+
+//       return this.transformResponse(sanitizedEntity);
+//     } catch (error) {
+  
+//       ctx.throw(500, 'Internal Server Error2', {
+//         details: error.message,
+//         stack: error.stack,
+//       });
+//     }
+//   },
+// }));
+
 'use strict';
 
-/**
- * browser-lite-page controller
- */
+
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::browser-lite-page.browser-lite-page', ({ strapi }) => ({
-  async find(ctx) {
-    try {
- 
-      const { locale = 'en' } = ctx.query;
-
-    
-      const entity = await strapi.entityService.findOne('api::browser-lite-page.browser-lite-page', {
-        locale,
-        populate: {
-          HeroSection: { populate: '*' },
-          IconsListSection: { populate: '*' },
-          BannerSection: { populate: '*' },
-          MediaWithTextSection: { populate: '*' },
-          BannerWithButtonSection: { populate: '*' },
-        },
-      });
-
-      if (!entity) {
-        return ctx.throw(404, `No content found for locale "${locale}"`);
-      }
-
-      
-      const { createdBy, updatedBy, ...sanitizedEntity } = entity;
-
-      return this.transformResponse(sanitizedEntity);
-    } catch (error) {
-  
-      ctx.throw(500, 'Internal Server Error2', {
-        details: error.message,
-        stack: error.stack,
-      });
-    }
-  },
-}));
+module.exports = createCoreController('api::browser-lite-page.browser-lite-page');
