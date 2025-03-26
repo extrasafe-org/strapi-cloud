d13d599062ead1d7a50edacbe0bf84a53324ce92
Yana <yanaegorova69.3@gmail.com>
2025-03-26 14:35:34 +0200

    update controller for browser lite

--- src/api/browser-lite-page/controllers/browser-lite-page.js
+++ src/api/browser-lite-page/controllers/browser-lite-page.js
@@ -6,41 +6,27 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-// module.exports = createCoreController('api::browser-lite-page.browser-lite-page', ({ strapi }) => ({
-//   async find(ctx) {
-//     // Fetch data with population
-//     const entities = await strapi.entityService.findMany('api::browser-lite-page.browser-lite-page', {
-//       populate: {
-//         HeroSection: {
-//           populate: '*', 
-//         },
-//       },
-//     });
+module.exports = createCoreController('api::browser-lite-page.browser-lite-page', ({ strapi }) => ({
+  async find(ctx) {
+    try {
+      const entity = await strapi.entityService.findOne('api::browser-lite-page.browser-lite-page', {
+        populate: {
+          HeroSection: {
+            populate: '*',
+          },
+        },
+      });
 
-//     // Remove unnecessary fields
-//     const sanitizedEntities = entities.map(entity => {
-//       const { createdBy, updatedBy, ...rest } = entity;
-//       return rest;
-//     });
+      if (!entity) {
+        ctx.throw(404, 'Not Found');
+      }
 
-//     return this.transformResponse(sanitizedEntities);
-//   },
+      const { createdBy, updatedBy, ...sanitizedEntity } = entity;
 
-//   async findOne(ctx) {
-//     const { id } = ctx.params;
+      return this.transformResponse(sanitizedEntity);
+    } catch (error) {
+      ctx.throw(500, 'Internal Server Error', { details: error.message });
+    }
+  },
 
-//     // Fetch data with population
-//     const entity = await strapi.entityService.findOne('api::browser-lite-page.browser-lite-page', id, {
-//       populate: {
-//         HeroSection: {
-//           populate: '*', 
-//         },
-//       },
-//     });
-
-//     // Remove unnecessary fields
-//     const { createdBy, updatedBy, ...sanitizedEntity } = entity;
-
-//     return this.transformResponse(sanitizedEntity);
-//   },
-// }));
+}));
