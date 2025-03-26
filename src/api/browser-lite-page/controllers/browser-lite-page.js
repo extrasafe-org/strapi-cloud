a2f486bfb4d3117ab106a4c0c0396a733b3ffdec
Yana <yanaegorova69.3@gmail.com>
2025-03-27 00:33:48 +0200

    populate HeroSection

--- src/api/browser-lite-page/controllers/browser-lite-page.js
+++ src/api/browser-lite-page/controllers/browser-lite-page.js
@@ -1,41 +1,45 @@
-'use strict';
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::browser-lite-page.browser-lite-page');
-
 // 'use strict';
-
-// /**
-//  * browser-lite-page controller
-//  */
 
 // const { createCoreController } = require('@strapi/strapi').factories;
 
-// module.exports = createCoreController('api::browser-lite-page.browser-lite-page', ({ strapi }) => ({
-//   async find(ctx) {
-//     try {
-//       // Минимальный запрос к Single Type без параметров
-//       const entity = await strapi.entityService.findOne('api::browser-lite-page.browser-lite-page', {
-//         populate: '*', // Популяция всех полей первого уровня
-//       });
+// module.exports = createCoreController('api::browser-lite-page.browser-lite-page');
 
-//       // Проверка результата
-//       if (!entity) {
-//         return ctx.throw(404, 'No content found');
-//       }
+'use strict';
 
-//       // Убираем системные поля
-//       const { createdBy, updatedBy, ...sanitizedEntity } = entity;
+/**
+ * browser-lite-page controller
+ */
 
-//       // Возвращаем отформатированный ответ
-//       return this.transformResponse(sanitizedEntity);
-//     } catch (error) {
-//       // Подробный вывод ошибки для диагностики
-//       return ctx.throw(500, 'Internal Server Error', {
-//         details: error.message,
-//         stack: error.stack,
-//       });
-//     }
-//   },
-// }));
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::browser-lite-page.browser-lite-page', ({ strapi }) => ({
+  async find(ctx) {
+    try {
+      const entity = await strapi.service('api::browser-lite-page.browser-lite-page').find({
+        populate: {
+          HeroSection: {
+            populate: '*',
+          },
+        },
+      });
+
+      if (!entity) {
+        return ctx.throw(404, 'No content found');
+      }
+
+      const sanitizedEntity = {
+        id: entity.id,
+        ...entity,
+        createdBy: undefined,
+        updatedBy: undefined,
+      };
+
+      return this.transformResponse(sanitizedEntity);
+    } catch (error) {
+      ctx.throw(500, 'Internal Server Error', {
+        details: error.message,
+        stack: error.stack,
+      });
+    }
+  },
+}));
