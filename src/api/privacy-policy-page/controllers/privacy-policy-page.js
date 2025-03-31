0b31c9ed6ef199e857204ee699df66582d3da969
Yana <yanaegorova69.3@gmail.com>
2025-03-31 19:32:58 +0300

    add populate

--- src/api/privacy-policy-page/controllers/privacy-policy-page.js
+++ src/api/privacy-policy-page/controllers/privacy-policy-page.js
@@ -6,4 +6,30 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::privacy-policy-page.privacy-policy-page');
+module.exports = createCoreController('api::privacy-policy-page.privacy-policy-page', ({ strapi }) => ({
+  async find(ctx) {
+    try {
+      const entity = await strapi.service('api::privacy-policy-page.privacy-policy-page').find({
+        populate: '*',
+      });
+
+      if (!entity) {
+        return ctx.throw(404, 'No content found');
+      }
+
+      const sanitizedEntity = {
+        id: entity.id,
+        ...entity,
+        createdBy: undefined,
+        updatedBy: undefined,
+      };
+
+      return this.transformResponse(sanitizedEntity);
+    } catch (error) {
+      ctx.throw(500, 'Internal Server Error', {
+        details: error.message,
+        stack: error.stack,
+      });
+    }
+  },
+}));
