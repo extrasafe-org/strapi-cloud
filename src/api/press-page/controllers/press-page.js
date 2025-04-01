0796c7d0ba547d185020178a57ddf7999727db11
Yana <yanaegorova69.3@gmail.com>
2025-04-01 17:03:50 +0300

    populate press page sections

--- src/api/press-page/controllers/press-page.js
+++ src/api/press-page/controllers/press-page.js
@@ -6,4 +6,40 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::press-page.press-page');
+module.exports = createCoreController('api::press-page.press-page', ({ strapi }) => ({
+  async find(ctx) {
+    try {
+      const entity = await strapi.service('api::press-page.press-page').find({
+        populate: {
+          MediaSection: {
+            populate: '*',
+          },
+           FeaturesSection: {
+            populate: '*',
+          },
+          FaqSection: {
+            populate:  '*',
+          },
+        },
+      });
+
+      if (!entity) {
+        return ctx.throw(404, 'No content found');
+      }
+
+      const sanitizedEntity = {
+        id: entity.id,
+        ...entity,
+        createdBy: undefined,
+        updatedBy: undefined,
+      };
+
+      return this.transformResponse(sanitizedEntity);
+    } catch (error) {
+      ctx.throw(500, 'Internal Server Error', {
+        details: error.message,
+        stack: error.stack,
+      });
+    }
+  },
+}));
