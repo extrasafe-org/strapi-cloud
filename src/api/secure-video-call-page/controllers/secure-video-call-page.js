913657116f69c11e6443dafd64b335e01a42b404
Yana <yanaegorova69.3@gmail.com>
2025-05-21 20:50:02 +0300

    add populate to secure video call

--- src/api/secure-video-call-page/controllers/secure-video-call-page.js
+++ src/api/secure-video-call-page/controllers/secure-video-call-page.js
@@ -6,4 +6,30 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::secure-video-call-page.secure-video-call-page');
+module.exports = createCoreController('api::secure-video-call-page.secure-video-call-page', ({ strapi }) => ({
+  async find(ctx) {
+    try {
+      const entity = await strapi.service('api::secure-video-call-page.secure-video-call-page').find({
+          populate: '*'
+      });
+
+      if (!entity) {
+        return ctx.throw(404, 'No content found');
+      }
+
+      const sanitizedEntity = {
+        id: entity.id,
+        ...entity,
+        createdBy: undefined,
+        updatedBy: undefined,
+      };
+
+      return this.transformResponse(sanitizedEntity);
+    } catch (error) {
+      ctx.throw(500, 'Internal Server Error', {
+        details: error.message,
+        stack: error.stack,
+      });
+    }
+  },
+}));
