c29b1f81f4bf78bcda4e95ee8c304b3bfc911951
Yana <yanaegorova69.3@gmail.com>
2025-05-28 10:39:43 +0300

    add populate to support page

--- src/api/support-page/controllers/support-page.js
+++ src/api/support-page/controllers/support-page.js
@@ -6,4 +6,34 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::support-page.support-page');
+module.exports = createCoreController('api::support-page.support-page', ({ strapi }) => ({
+  async find(ctx) {
+    try {
+      const entity = await strapi.service('api::support-page.support-page').find({
+        populate: {
+          FaqTabSection: {
+             populate: '*'
+          },
+        },
+      });
+
+      if (!entity) {
+        return ctx.throw(404, 'No content found');
+      }
+
+      const sanitizedEntity = {
+        id: entity.id,
+        ...entity,
+        createdBy: undefined,
+        updatedBy: undefined,
+      };
+
+      return this.transformResponse(sanitizedEntity);
+    } catch (error) {
+      ctx.throw(500, 'Internal Server Error', {
+        details: error.message,
+        stack: error.stack,
+      });
+    }
+  },
+}));
