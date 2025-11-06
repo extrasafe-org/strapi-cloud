9588faf4ab7bed96fbb2119ab38713087c1b80f9
Yana <yanaegorova69.3@gmail.com>
2025-11-06 15:39:33 +0200

    add env variables

--- config/plugins.js
+++ config/plugins.js
@@ -1,11 +1,41 @@
-module.exports = {
-	'multi-select': {
-		enabled: true,
-		resolve: './src/plugins/multi-select',
-	},
-// 	  'clear-cache': {
+// module.exports = {
+// 	'multi-select': {
+// 		enabled: true,
+// 		resolve: './src/plugins/multi-select',
+// 	},
+// // 	  'clear-cache': {
+// //     enabled: true,
+// //     resolve: './src/plugins/clear-cache'
+// 	//   },
+//   headlockr: {
 //     enabled: true,
-//     resolve: './src/plugins/clear-cache'
+//     config: {
+//       licenseKey: env('HEADLOCKR_LICENSE_KEY'),
+//       apiKey: env('HEADLOCKR_API_KEY'),
+//       apiSecret: env('HEADLOCKR_API_SECRET'),
+//     },
 //   },
-};
+// };
 
+module.exports = ({ env }) => {
+    console.log('🧩 HeadLockr loaded with SECRET (first 8 chars):', env('HEADLOCKR_API_SECRET')?.slice(0, 8));
+  console.log('🧩 HeadLockr loaded with API_KEY:', env('HEADLOCKR_API_KEY'));
+ return {
+  'multi-select': {
+    enabled: true,
+    resolve: './src/plugins/multi-select',
+  },
+  // 'clear-cache': {
+  //   enabled: true,
+  //   resolve: './src/plugins/clear-cache',
+  // },
+  headlockr: {
+    enabled: true,
+    config: {
+      licenseKey: env('HEADLOCKR_LICENSE_KEY'),
+      apiKey: env('HEADLOCKR_API_KEY'),
+      apiSecret: env('HEADLOCKR_API_SECRET'),
+    },
+  },
+}
+};
